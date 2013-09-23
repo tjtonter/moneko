c133f731fbf365c68176aa8f65882554e7d252b1
Timo Tonteri <timo.tonteri@helsinki.fi>
2013-09-23 04:48:11 +0300

    New rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moneko::Application.config.secret_key_base = '9cb08849f3bd9c963a515f9c3d6bfac5e07806383905ad2e1323fcc38783e0bd0f882396734e0cefdbf069f13dcc19b034278712f499ae75518ef7c7af10da7b'
