c2af160d2747f0af928dba633a229c0a0a4ccd29
Timo Tonteri <timo.tonteri@helsinki.fi>
2013-11-06 20:48:01 +0200

    Users controller change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(user_params)
+
+    if @user.save
+        redirect_to root_url, :notice => "Rekisteröity!"
+    else
+        render "new"
+    end
   end
+
+  private
+    def user_params
+        params.require(:user).permit(:email, :username, :name)
+    end
 end
