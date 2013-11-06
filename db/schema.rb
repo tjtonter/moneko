b86f42b60b66d43341e3db9759391b5f7fcf2553
Timo Tonteri <timo.tonteri@helsinki.fi>
2013-11-06 18:17:11 +0200

    ADDED: User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130926205827) do
+ActiveRecord::Schema.define(version: 20131106161039) do
 
   create_table "offers", force: true do |t|
     t.text     "customer"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20130926205827) do
     t.text     "execution"
     t.text     "delivery"
     t.text     "charge"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "address"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
