86214bc83f32213adaae5e6bfb964d663dd35242
Timo Tonteri <timo.tonteri@helsinki.fi>
2013-11-12 19:38:55 +0200

    MISC

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,6 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.create(user_params)
-
-    if @user.save
-        redirect_to root_url, :notice => "Rekisteröity!"
-    else
-        render "new"
-    end
+  def index
+    @users = User.all
   end
 
   private
