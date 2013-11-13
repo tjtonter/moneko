c7cceecefdfc1e3d9761374392252c6442963894
Timo Tonteri <timo.tonteri@helsinki.fi>
2013-11-13 11:09:14 +0200

    ADDED: User signout and list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   before_filter :update_sanitized_params, if: :devise_controller?
 
   def update_sanitized_params
-    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :name, :password)}
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :name, :password, :password_confirmation)}
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+      @user = User.find(params[:id])
+  end
+
   private
     def user_params
         params.require(:user).permit(:email, :username, :name)
