984a96143743f6277811ea25c8e633b880d12b36
Timo Tonteri <timo.tonteri@helsinki.fi>
2013-11-13 11:42:48 +0200

    ADDED: Order model

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,6 +1,6 @@
 class Offer < ActiveRecord::Base
   include ActionView::Helpers::TextHelper
-
+  has_many :orders
   validates :customer, :presence => true
   validates :contents, :presence => true 
 
