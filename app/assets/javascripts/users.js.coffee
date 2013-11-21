a612102cb45e253f4425e04822f5fb2e5b050dc6
Timo Tonteri <timo.tonteri@helsinki.fi>
2013-11-21 03:44:57 +0200

    CHANGED: Autocomplete.

--- app/assets/javascripts/users.js.coffee
+++ app/assets/javascripts/users.js.coffee
@@ -2,4 +2,4 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 $ ->
-  $("#job_order_id").autocomplete source: "/orders"
+  $("#job_order_id").autocomplete(source: "/orders.json")
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,11 @@
 class OrdersController < ApplicationController
   before_filter :load_offer_id
   def index
-    @orders = params[:term] ? Order.where('description LIKE (?)', "%#{params[:term]}%") : Order.all
+    @orders = params[:term] ? Order.where('title LIKE (?)', "%#{params[:term]}%") : Order.all
   
     respond_to do |format|
       format.html
-      format.json { render json: @orders.map { |o| o.number } }
+      format.json { render json: @orders.map {|o| o.id} }
     end
   end
 
