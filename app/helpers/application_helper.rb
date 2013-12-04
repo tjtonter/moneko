ba418fbfffaf475635f71a608aaa6eeefa5f122c
Timo Tonteri <timo.tonteri@helsinki.fi>
2013-12-04 11:03:07 +0200

    ADDED: Status label to orders index.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def status_to_class(status)
+    case status
+    when "waiting"
+      return "info"
+    when "active"
+      return "danger"
+    when "complete"
+      return "success"
+    when "billed"
+      return "primary"
+    end
+  end
 end
