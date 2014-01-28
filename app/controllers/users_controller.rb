68234399c41e3f390372766e0beab57cfa826be8
Timo Tonteri <timo.tonteri@helsinki.fi>
2014-01-28 15:05:14 +0200

    ADDED: Job table sort.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,23 @@
 class UsersController < ApplicationController
+  helper_method :sort_column, :sort_direction
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
-    @jobs = @user.jobs.order(params[:sort] + ' ' + params[:direction])
+    @jobs = @user.jobs.order(sort_column + ' ' + sort_direction)
     @orders = Order.all
   end
 
   private
     def user_params
       params.require(:user).permit(:email, :username, :name)
+    end
+    def sort_column
+      Job.column_names.include?(params[:sort]) ? params[:sort] : "id"
+    end
+    def sort_direction
+      %[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
   end
   def sortable(column, title = nil)
     title ||= column.titleize
-    direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
+    direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
     link_to title, :sort => column, :direction => direction
   end
 end
