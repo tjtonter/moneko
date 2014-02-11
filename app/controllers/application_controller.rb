6336178acbb75cce4cd4ff8414d452879e397fc9
Timo Tonteri <timo.tonteri@helsinki.fi>
2014-02-11 23:55:59 +0200

    ADDED: Devise before_filter to site-wide authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :update_sanitized_params, if: :devise_controller?
+  before_filter :authenticate_user!
   attr_accessor :login
 
   protected
