c991e88c225e0ed38a1b260c906a9b8e9da2fbf6
Timo Tonteri <timo.tonteri@helsinki.fi>
2014-02-19 09:39:46 +0200

    FIXED: Calendar integration.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,15 @@
 class TasksController < ApplicationController
   def index
-    @tasks = User.find(params[:user_id]).tasks
+    @user = User.find(params[:user_id])
+    if params[:start] && params[:end]
+      @orders = @user.orders.where("begin_at < ? AND end_at > ?", 
+                                   Time.at(params[:end].to_i), Time.at(params[:start].to_i))
+    else
+      @orders = @user.orders
+    end
     respond_to do |format|
       format.html
-      format.json { render json: custom_json(@tasks) }
+      format.json { render json: custom_json(@orders) }
     end
   end
 
@@ -11,14 +17,14 @@ class TasksController < ApplicationController
   end
 
   private
-  def custom_json(tasks)
-    list = tasks.map do |t|
-      { :id => t.id,
-        :title => t.order.title,
-        :start => t.order.begin_at,
-        :end => t.order.end_at,
-        :url => new_user_job_path(params[:user_id]) + "?order_id=#{t.order_id}",
-        :allDay => (t.order.begin_at.day == t.order.end_at.day ? false : true)
+  def custom_json(orders)
+    list = orders.map do |o|
+      { :id => o.id,
+        :title => o.title,
+        :start => o.begin_at,
+        :end => o.end_at,
+        :url => new_user_job_path(params[:user_id]) + "?order_id=#{o.id}",
+        :allDay => (o.begin_at.day == o.end_at.day ? false : true)
       }
     end
     list.as_json
