06d772e34f57505e1a5e9cb3926583e7ca2839d8
root <root@moneko.fi>
2014-03-31 01:50:42 +0300

    ADDED: Precision and scale to migration.

--- db/migrate/20140330221027_change_duration_format_in_jobs.rb
+++ db/migrate/20140330221027_change_duration_format_in_jobs.rb
@@ -1,8 +1,3 @@
 class ChangeDurationFormatInJobs < ActiveRecord::Migration
-  def up
-    change_column :jobs, :duration, :decimal
-  end
-  def down
-    change_column :jobs, :duration, :time
-  end
+  change_column :jobs, :duration, :decimal, precision: 8, scale: 2
 end
