490b5fc2196829c1075571233e7fbdefdf5f0bd1
Timo Tonteri <timo.tonteri@helsinki.fi>
2014-03-31 01:15:21 +0300

    CHANGED: jobs duration column to decimal.

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,51 +1,4 @@
 class OffersController < ApplicationController
-  class OfferReport < Prawn::Document
-    include  ActionView::Helpers::SanitizeHelper
-
-    LEFT = 35
-    def initialize(options = {})
-      super(:top_margin => 100, :bottom_margin => 100)
-    end
-    def to_pdf(offer)
-      text offer.customer, :inline_format => true
-      move_down 20
-      text "TARJOUDUMME SUORITTAMAAN SIIVOUSTA SEURAAVASTI", :style => :bold
-      move_down 20
-      data = [
-      ["Kohde", offer.target],
-      ["Tarjouksemme sisältö", sanitize(offer.contents, tags: %w(b i u))],
-      ["Työn suoritus", sanitize(offer.execution, tags: %w(b i u))],
-      ["Toimitusaika", sanitize(offer.delivery, tags: %w(b i u))],
-      ["Palvelumaksu", sanitize(offer.charge, tags: %w(b i u))]
-      ]
-      table(data, :cell_style => { :inline_format => true} ) do
-        cells.borders = []
-        cells.padding = 5
-        column(0).font_style = :bold
-      end
-
-      canvas do
-        repeat(:all) do
-          draw_text "Raimo Saariaho", :at => [LEFT, bounds.top-60]
-          draw_text "PALVELUTARJOUS", :at => [400, bounds.top-30]
-          draw_text "Luottamuksellinen", :at => [400, bounds.top-45]
-          draw_text "29.10.2013", :at => [400, bounds.top-60]
-          font_size 10
-          draw_text "Moneko oy", :at => [LEFT, 50]
-          draw_text "Y-tunnus: 123456-Y", :at => [LEFT, 35]
-          fill_color "fb8900"
-          draw_text "MONEKO",
-            :at => [LEFT, bounds.top-35],
-            :style => :bold, 
-            :size => 20
-          move_down 70
-          stroke_horizontal_rule
-        end
-      end
-      render
-    end
-  end
-
   def index
     @offers = Offer.all
   end
@@ -69,8 +22,9 @@ class OffersController < ApplicationController
     respond_to do |format|
       format.html
       format.pdf do
-        pdf = OfferReport.new
-        send_data pdf.to_pdf(@offer), filename: "hello.pdf", type: "application/pdf", disposition: "inline"
+        pdf = OfferPdf.new(@offer, view_context)
+        send_data pdf.render, filename: "#{@offer.target}.pdf",
+          type: "application/pdf", disposition: "inline"
       end
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140319170507) do
+ActiveRecord::Schema.define(version: 20140330221027) do
 
   create_table "jobs", force: true do |t|
     t.integer  "order_id"
