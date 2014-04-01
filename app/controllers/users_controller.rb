fb04cb243ac53ef99ce93b2799c60225eb569259
Timo Tonteri <timo.tonteri@helsinki.fi>
2014-04-02 00:54:23 +0300

    FIXED: Jobs pdf link.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @today = Date.today
+    if @today.day.between?(16,31)
+      @past = Date.new(@today.year, @today,month, 1)
+    else
+      @past = @today-1.month
+      @past = Date.new(@past.year, @past.month, 16)
+    end
   end
 
   def show
@@ -8,7 +15,7 @@ class UsersController < ApplicationController
     @jobs = @user.jobs.all
     @orders = Order.all
   end
-
+  
   private
     def user_params
       params.require(:user).permit(:email, :username, :name)
