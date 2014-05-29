8d6b7e027e467b6ab4950be6d413ba9b1e78d33c
Timo Tonteri <timo.tonteri@helsinki.fi>
2014-05-29 16:03:38 +0300

    CHANGED: Users abilities.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,8 @@ class Ability
   def initialize(user)
     user ||= User.new
     can :manage, Order if user.is?(:admin)
+    can :manage, Offer if user.is?(:admin)
+    can :manage, User if user.is?(:admin)
     can :manage, User if user.is?(:user)
     if !user.is?(:admin)
       cannot [:create, :update], User
