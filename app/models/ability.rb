898a7ce254e4594eb1cc7b445cccd610b8b3a8dd
Timo Tonteri <timo.tonteri@helsinki.fi>
2014-07-30 10:32:09 +0300

    CHANGED: Abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,9 +5,9 @@ class Ability
     user ||= User.new
     can :manage, Order if user.is?(:admin)
     can :manage, User if user.is?(:user)
+    can :manage, Offer if user.is?(:admin)
     if !user.is?(:admin)
       cannot [:create, :update], User
-      cannot :manage, Offer
     end
   end
 end
