76f85003dc58f106bf2451c94c0a6c4ecc9f474c
Timo Tonteri <timo@moneko.fi>
2015-01-26 13:00:26 +0200

    Simplified abilities.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new
     if user.is?(:admin)
-      can :manage, Order
-      can :manage, Offer
+      can :manage, :all
     end
     if user.is?(:user)
       cannot [:create, :update], User
