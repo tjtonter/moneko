bcb7f6efd9745a7847a9e84034035701738824f6
Timo Tonteri <timo@moneko.fi>
2015-02-18 01:05:16 +0200

    Removed occurrences controller.

--- app/controllers/occurrences_controller.rb
+++
@@ -1,9 +0,0 @@
-class OccurrencesController < ApplicationController
-  def index
-    @occurrences = Occurrence.where("start < ? AND end > ?", params[:end], params[:start])
-    respond_to do |format|
-      format.html
-      format.json { render json: @occurrences.as_json }
-    end
-  end
-end
