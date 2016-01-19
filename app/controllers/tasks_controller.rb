c9ea0c310b2cb8821aa9e094ae8c4bcbfb039eb0
Timo Tonteri <timo@moneko.fi>
2016-01-19 16:51:01 +0200

    Tuned the user tasks search.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,14 @@
 class TasksController < ApplicationController
   def index
     @user = User.find(current_user)
-    @orders = @user.orders
+    if params[:end] && params[:start]
+      t1 = Date.strptime params[:start], "%s"
+      t2 = Date.strptime params[:end], "%s"
+      @orders = @user.orders.where("begin_at < ? AND (end_at > ? OR until_at > ?)", t2, t1, t1)
+    else
+      @orders = @user.orders
+    end
+    #@orders = @user.orders
     respond_to do |format|
       format.html
       format.json { render json: @orders }
