de8ff686b3898679e5bff5e5ee768b3202621c4f
Timo Tonteri <timo@moneko.example.com>
2016-02-10 16:27:59 +0200

    Fixed duration field

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,4 +4,8 @@ class Job < ActiveRecord::Base
   validates_presence_of :date, :begin, :end
 #  validates :duration, :format => { :with => /\d+(\.\d+)/, 
 #    :message => "Anna kesto muodossa 0..24 tai 0.5..23.5" }
+  def as_hours
+    msec = self.end-self.begin
+    msec/3600
+  end
 end
