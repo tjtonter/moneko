6b876695b6de1d9ee266a34eab9d4f5380befcf4
root <root@moneko.example.com>
2016-02-29 18:38:49 +0200

    Added google environment variable

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,6 @@
 require File.expand_path('../application', __FILE__)
 #RAILS_ENV = 'development'
 RAILS_ENV = 'production'
+ENV['GOOGLE_APPLICATION_CREDENTIALS']="/var/www/moneko/client_secrets.json"
 # Initialize the Rails application.
 Moneko::Application.initialize!
